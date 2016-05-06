50eb6c0b56184b94e10b8c0d6158c46c4fbf53cb
Sergey Kanukov <s.kanukov@gmail.com>
2016-05-06 15:15:58 +0300

    Add role to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506120542) do
+ActiveRecord::Schema.define(version: 20160506121047) do
 
   create_table "entries", force: :cascade do |t|
     t.integer  "feed_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160506120542) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
