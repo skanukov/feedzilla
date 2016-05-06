7eeb327393f9b91be18bb184b9bf39a17b21dc6c
Sergey Kanukov <s.kanukov@gmail.com>
2016-05-06 15:46:44 +0300

    Create basic Admin and Feed controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'entries#index'
+
   devise_for :users
   resources :entries
   resources :feeds
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'entries#index'
+
+  namespace :admin do
+    resources :feeds
+  end
 end
---
+++ test/controllers/admin/feeds_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::FeedsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
