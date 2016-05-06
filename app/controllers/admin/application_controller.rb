56c4bc0f9cdc997a4139fee7ffe8c4049e912bdf
Sergey Kanukov <s.kanukov@gmail.com>
2016-05-06 15:58:04 +0300

    Add authorization

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,2 +1,11 @@
 class Admin::ApplicationController < ApplicationController
+  before_action :authenticate_admin!
+
+  private
+
+  def authenticate_admin!
+    authenticate_user!
+    return if current_user.admin?
+    redirect_to root_path, alert: t('authorization.unauthorized_access')
+  end
 end
